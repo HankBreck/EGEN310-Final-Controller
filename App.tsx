2c26b96d3f928944ea612eeab7c5f20511df69d6
Hank Breckenridge <breckenridgeh2@gmail.com>
2023-03-28 23:12:11 -0600

    Maybe working connection

--- App.tsx
+++ App.tsx
@@ -1,13 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WifiManager from 'react-native-wifi-reborn';
+
+const WIFI_SSID = "esp32-wifi"
+const WIFI_PASS = "abc123"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [isConnected, setIsConnected] = React.useState(false)
+    // https://github.com/JuanSeBestia/react-native-wifi-reborn
+    const connect = async () => {
+        await WifiManager.connectToProtectedSSID(WIFI_SSID, WIFI_PASS, false).then(onConnectSuccess, onConnectFail)
+    }
+
+    useEffect(() => {
+        connect()
+    }, [])
+
+    const onConnectSuccess = () => {
+        setIsConnected(true)
+    }
+    
+    const onConnectFail = () => {
+        setIsConnected(false)
+    }
+
+    if (isConnected) {
+        return (
+            <View style={styles.container}>
+                <Text>WiFi is Connected!</Text>
+            </View>
+        )
+    } else {
+        return (
+            <View style={styles.container}>
+                <Text>Unable to connect to WiFi</Text>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
